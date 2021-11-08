cf1ecdc7e3e893f2ae6887adb6ca160a0854aee5
junhp7 <junhp1994@gmail.com>
2021-11-08 22:38:52 +0900

    Project responsive

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,15 +4,19 @@ import styles from './footer.module.css';
 const Footer = () => (
         <footer className={styles.footer}>
             <ul className={styles.copyright}>
+                <li>본 사이트는 상업적 목적이 아닌 개인 포트폴리오 사이트로 제작되었습니다. 사용된 일부 이미지 및 폰트 등은 별도의 출처가 있음을 밝힙니다.</li>
                 <li>Copyright2021. Junhyung Park. All rights reserved.</li>
-                <li>본 사이트는 상업적 목적이 아닌 개인 포트폴리오 사이트로 제작되었습니다.</li>
-                <li>사용된 일부 이미지 및 폰트 등은 별도의 출처가 있음을 밝힙니다.</li>
             </ul>
             <div className={styles.ref}>
-                <span>by</span>
-                <img className={styles.img} src={require('../../static/images/footer_ape.svg').default} alt="iconape" />
-                <img className={styles.img} src={require('../../static/images/footer_fla.svg').default} alt="flaticon" />
-                <img className={styles.img} src={require('../../static/images/footer_freepik.svg').default} alt="freepik" />
+                <a href="https://iconape.com/" target="_blank"  rel="noreferrer" className={styles.link}>
+                    <img src={require('../../static/images/footer_ape.svg').default} alt="iconape" />
+                </a>
+                <a href="https://www.flaticon.com/" target="_blank"  rel="noreferrer" className={styles.link}>
+                    <img src={require('../../static/images/footer_fla.svg').default} alt="flaticon" />
+                </a>
+                <a href="https://www.freepik.com/" target="_blank"  rel="noreferrer" className={styles.link}>
+                    <img src={require('../../static/images/footer_freepik.svg').default} alt="freepik" />
+                </a>
             </div>
         </footer>    
     );
