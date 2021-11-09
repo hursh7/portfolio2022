6183bed7107622ada80393e79c58d1f6065a0123
junhp7 <junhp1994@gmail.com>
2021-11-09 15:15:05 +0900

    add toggle menu

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './header.module.css';
 
 const Header = ({ onReset }) => {
-    
+    const [dropDown, setDropDown] = useState(false);
+
+    const onToggle= () => {
+      setDropDown(dropDown => !dropDown);
+      console.log(`${dropDown}`);
+    }
+    const toggle = dropDown === false ? styles.show : styles.hide;
+
     return (
         <header className={styles.header}>
             <p href="" className={styles.logo} onClick={() => onReset()}>
@@ -22,6 +29,13 @@ const Header = ({ onReset }) => {
                     <a href="#home">Resume</a>
                 </li>
             </ul>
+            <div className={`${styles.show} ${toggle}`} onClick={() => onToggle()} >
+                <div className={styles.trigger}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
         </header>
     )
 };
--- src/components/side_menu/side_menu.jsx
+++ src/components/side_menu/side_menu.jsx
@@ -6,16 +6,22 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 import styles from './side_menu.module.css';
 
 library.add(fab, fas);
-const SideMenu = (props) => (
+const SideMenu = () => (
         <ul className={styles.nav}>
             <li>
-                <FontAwesomeIcon icon={["fab", "github-square"]} />
+                <a href="https://github.com/hursh7" target="_blank"  rel="noreferrer">
+                    <FontAwesomeIcon icon={["fab", "github-square"]} />
+                </a>
             </li>
             <li>
-                <FontAwesomeIcon icon={["fab", "blogger-b"]} />
+                <a href="https://github.com/hursh7" target="_blank"  rel="noreferrer">
+                    <FontAwesomeIcon icon={["fab", "blogger-b"]} />
+                </a>
             </li>
             <li>
-                <FontAwesomeIcon icon={["fab", "google"]} />
+                <a href="mailto:junhp1994@gmail.com" target="_blank"  rel="noreferrer">
+                    <FontAwesomeIcon icon={["fab", "google"]} />
+                </a>
             </li>
         </ul>
     );
