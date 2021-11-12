e7ed32839b7b76197778c4798c65661b4c7a3714
junhp7 <junhp1994@gmail.com>
2021-11-12 16:29:27 +0900

    add project img

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -8,9 +8,7 @@ const Project = forwardRef((props, ref) => {
                     <h2 className={styles.index}>Projects</h2>
                     <div className={styles.container}>
                          <div className={styles.item}>
-                              <div className={styles.canvas}>
-                                   {/* <img className={styles.img} src={require('../../static/images/test.jpg').default} alt="포트폴리오 이미지" /> */}
-                              </div>
+                              <div className={`${styles.canvas} ${styles.canvas01}`}></div>
                               <div className={styles.text}>
                               <div className={styles.number}>01</div>
                                    <h3 className={styles.title}>CardMaker</h3>
@@ -36,7 +34,31 @@ const Project = forwardRef((props, ref) => {
                                    </div>
                               </div>
                          </div>
-                         <div className={styles.item}>2</div>
+                         <div className={styles.item}>
+                              <div className={`${styles.canvas} ${styles.canvas02}`}></div>
+                              <div className={styles.text}>
+                              <div className={styles.number}>02</div>
+                                   <h3 className={styles.title}>Youtube (CloneCoding)</h3>
+                                   <p className={styles.introduce}>
+                                        사용자의 정보를 실시간으로 업데이트 할 수 있는 반응형 명함 어플리케이션입니다. React와 React Router로 개발하였습니다. Firebase의 Auth API를 통해 소셜 미디어 로그인 기능을 구현하고, Real Time Database를 이용해 데이터를 실시간으로 연동하였습니다.   
+                                   </p>
+                                   <div className={styles.tecbox}>
+                                        <div className={styles.tec}>#React</div>
+                                        <div className={styles.tec}>#PostCSS</div>
+                                        <div className={styles.tec}>#Postman</div>
+                                   </div>
+                                   <div className={styles.linkbox}>
+                                        <a href="https://github.com/hursh7/business-card-maker" target="_blank"  rel="noreferrer" className={styles.link}>
+                                             <img className={styles.icon} src={require('../../static/images/link_github.svg').default} alt="git" />
+                                             <span className={styles.path}>Github</span>
+                                        </a>
+                                        <a href="https://hursh7.github.io/business-card-maker/" target="_blank"  rel="noreferrer" className={styles.link}>
+                                             <img className={styles.icon} src={require('../../static/images/link_page.svg').default} alt="page" />
+                                             <p className={styles.path}>Page</p>
+                                        </a>
+                                   </div>
+                              </div>
+                         </div>
                          <div className={styles.item}>3</div>
                     </div>
                </section>     
