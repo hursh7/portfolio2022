376ffbe339ac87bac50f411f07349186478f6d53
junhp7 <junhp1994@gmail.com>
2021-11-16 20:25:16 +0900

    footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -9,30 +9,8 @@ library.add(fab, fas);
 const Footer = () => (
         <footer className={styles.footer}>
             <p href="" className={styles.logo}>
-                Jun .
+                FrontEnd Developer Jun.
             </p>
-            <ul className={styles.link}>
-                <li>
-                    <a href="https://github.com/hursh7" target="_blank"  rel="noreferrer">
-                        <FontAwesomeIcon icon={["fab", "github"]} />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://github.com/hursh7" target="_blank"  rel="noreferrer">
-                        <FontAwesomeIcon icon={["fas", "comment-dots"]} />
-                    </a>
-                </li>
-                <li>
-                    <a href="mailto:junhp1994@gmail.com" target="_blank"  rel="noreferrer">
-                        <FontAwesomeIcon icon={["fab", "google"]} />
-                    </a>
-                </li>
-                <li>
-                    <a href="mailto:junhp1994@gmail.com" target="_blank"  rel="noreferrer">
-                        <FontAwesomeIcon icon={["fas", "id-badge"]} />
-                    </a>
-                </li>
-            </ul>
             <ul className={styles.text}>
                 <li>본 사이트는 상업적 목적이 아닌 개인 포트폴리오 사이트로 제작되었습니다.</li>
                 <li>사용된 일부 이미지 및 폰트 등은 별도의 출처가 있음을 밝힙니다.</li>
@@ -48,7 +26,31 @@ const Footer = () => (
                     <img src={require('../../static/images/footer_freepik.svg').default} alt="freepik" />
                 </a>
             </div>
-            <p className={styles.copyright}>&copy; 2021 Junhyung Park. All rights reserved.</p>
+            <div className={styles.box}>
+                <p className={styles.copyright}>&copy; 2021 Junhyung Park. All rights reserved.</p>
+                <ul className={styles.link}>
+                    <li>
+                        <a href="https://github.com/hursh7" target="_blank"  rel="noreferrer">
+                            <FontAwesomeIcon icon={["fab", "github"]} />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/hursh7" target="_blank"  rel="noreferrer">
+                            <FontAwesomeIcon icon={["fas", "comment-dots"]} />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="mailto:junhp1994@gmail.com" target="_blank"  rel="noreferrer">
+                            <FontAwesomeIcon icon={["fab", "google"]} />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="mailto:junhp1994@gmail.com" target="_blank"  rel="noreferrer">
+                            <FontAwesomeIcon icon={["fas", "id-badge"]} />
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </footer>    
     );
 
--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -40,7 +40,7 @@ const Project = forwardRef((props, ref) => {
                               <div className={styles.number}>02</div>
                                    <h3 className={styles.title}>Youtube (CloneCoding)</h3>
                                    <p className={styles.introduce}>
-                                        사용자의 정보를 실시간으로 업데이트 할 수 있는 반응형 명함 어플리케이션입니다. React와 React Router로 개발하였습니다. Firebase의 Auth API를 통해 소셜 미디어 로그인 기능을 구현하고, Real Time Database를 이용해 데이터를 실시간으로 연동하였습니다.   
+                                        유튜브 클론코딩입니다. React로 개발 하였고, netlify로 배포 하였습니다. 포스트맨 툴을 사용해 유튜브 APIs 데이터를 받아와서 인기 동영상 목록 보여주기, 검색 기능 등을 구현 하였습니다.   
                                    </p>
                                    <div className={styles.tecbox}>
                                         <div className={styles.tec}>#React</div>
@@ -48,18 +48,42 @@ const Project = forwardRef((props, ref) => {
                                         <div className={styles.tec}>#Postman</div>
                                    </div>
                                    <div className={styles.linkbox}>
-                                        <a href="https://github.com/hursh7/business-card-maker" target="_blank"  rel="noreferrer" className={styles.link}>
+                                        <a href="https://github.com/hursh7/youtube-clone" target="_blank"  rel="noreferrer" className={styles.link}>
                                              <img className={styles.icon} src={require('../../static/images/link_github.svg').default} alt="git" />
                                              <span className={styles.path}>Github</span>
                                         </a>
-                                        <a href="https://hursh7.github.io/business-card-maker/" target="_blank"  rel="noreferrer" className={styles.link}>
+                                        <a href="https://hursh7-juntube.netlify.app/" target="_blank"  rel="noreferrer" className={styles.link}>
                                              <img className={styles.icon} src={require('../../static/images/link_page.svg').default} alt="page" />
                                              <p className={styles.path}>Page</p>
                                         </a>
                                    </div>
                               </div>
                          </div>
-                         <div className={styles.item}>3</div>
+                         <div className={styles.item}>
+                              <div className={`${styles.canvas} ${styles.canvas02}`}></div>
+                              <div className={styles.text}>
+                              <div className={styles.number}>02</div>
+                                   <h3 className={styles.title}>Youtube (CloneCoding)</h3>
+                                   <p className={styles.introduce}>
+                                        유튜브 클론코딩입니다. React로 개발 하였고, netlify로 배포 하였습니다. 포스트맨 툴을 사용해 유튜브 APIs 데이터를 받아와서 인기 동영상 목록 보여주기, 검색 기능 등을 구현 하였습니다.   
+                                   </p>
+                                   <div className={styles.tecbox}>
+                                        <div className={styles.tec}>#React</div>
+                                        <div className={styles.tec}>#PostCSS</div>
+                                        <div className={styles.tec}>#Postman</div>
+                                   </div>
+                                   <div className={styles.linkbox}>
+                                        <a href="https://github.com/hursh7/youtube-clone" target="_blank"  rel="noreferrer" className={styles.link}>
+                                             <img className={styles.icon} src={require('../../static/images/link_github.svg').default} alt="git" />
+                                             <span className={styles.path}>Github</span>
+                                        </a>
+                                        <a href="https://hursh7-juntube.netlify.app/" target="_blank"  rel="noreferrer" className={styles.link}>
+                                             <img className={styles.icon} src={require('../../static/images/link_page.svg').default} alt="page" />
+                                             <p className={styles.path}>Page</p>
+                                        </a>
+                                   </div>
+                              </div>
+                         </div>
                     </div>
                </section>     
           </div>
