b7947f4d98811e718da5ee507df9740a923bb6ca
junhp7 <junhp1994@gmail.com>
2021-12-24 16:37:55 +0900

    add link

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,27 +2,29 @@ import React from 'react';
 import styles from './main.module.css';
 
 const Main = ({ useScrollFadeIn }) => {
-    const animatedItem = {
-        0: useScrollFadeIn('down', 1, 0),
-        1: useScrollFadeIn('down', 1, 0.1),
-    };
+  const animatedItem = {
+    0: useScrollFadeIn('down', 1, 0),
+    1: useScrollFadeIn('down', 1, 0.1),
+  };
 
-    return (
-        <section className={styles.main}>
-            <div className={styles.text} {...animatedItem[0]}>
-                <div className={styles.title}>
-                    <p className={styles.name}>Hi, i am Junhyung Park</p>
-                    <h1 className={styles.position}>FrontEnd Developer</h1>
-                </div>
-                <p class={styles.introduce}>
-                    안녕하세요 2년차 퍼블리셔 및 신입 프론트엔드 개발자 박준형입니다. <br /> 
-                    웹접근성과 웹표준을 준수하여 퍼블리싱하고, <br /> 
-                    최신 문법의 자바스크립트(ES6+)와 리액트를 통해 개발합니다.
-                </p>
-                <button className={styles.button}>SEE MY RESUME</button>
-            </div>
-            <div className={styles.img}{...animatedItem[1]}></div>
-        </section>
-    )};
+  return (
+    <section className={styles.main}>
+      <div className={styles.text} {...animatedItem[0]}>
+        <div className={styles.title}>
+          <p className={styles.name}>Hi, i am Junhyung Park</p>
+          <h1 className={styles.position}>FrontEnd Developer</h1>
+        </div>
+        <p class={styles.introduce}>
+          안녕하세요 2년차 퍼블리셔 및 신입 프론트엔드 개발자 박준형입니다.{' '}
+          <br />
+          웹접근성과 웹표준을 준수하여 퍼블리싱하고, <br />
+          최신 문법의 자바스크립트(ES6+)와 리액트를 통해 개발합니다.
+        </p>
+        <button className={styles.button}>SEE MY RESUME</button>
+      </div>
+      <div className={styles.img} {...animatedItem[1]}></div>
+    </section>
+  );
+};
 
 export default Main;
--- src/components/side_menu/side_menu.jsx
+++ src/components/side_menu/side_menu.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import styles from './side_menu.module.css';
 
-const SideMenu = ({FontAwesomeIcon}) => (
-        <ul className={styles.link}>
-            <li>
-                <a href="https://github.com/hursh7" target="_blank"  rel="noreferrer">
-                    <FontAwesomeIcon icon={["fab", "github"]} />
-                </a>
-            </li>
-            <li>
-                <a href="https://sudsy-skink-5a5.notion.site/JS-94f29388c3be48cba2e921db41ff45ee" target="_blank"  rel="noreferrer">
-                    <FontAwesomeIcon icon={["fas", "comment-dots"]} />
-                </a>
-            </li>
-            <li>
-                <a href="mailto:junhp1994@gmail.com" target="_blank"  rel="noreferrer">
-                    <FontAwesomeIcon icon={["fas", "id-badge"]} />
-                </a>
-            </li>
-        </ul>
-    );
+const SideMenu = ({ FontAwesomeIcon }) => (
+  <ul className={styles.link}>
+    <li>
+      <a href='https://github.com/hursh7' target='_blank' rel='noreferrer'>
+        <FontAwesomeIcon icon={['fab', 'github']} />
+      </a>
+    </li>
+    <li>
+      <a
+        href='https://www.notion.so/junhp/205e0379c9444d30b32de2ddb7672ac5'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <FontAwesomeIcon icon={['fas', 'comment-dots']} />
+      </a>
+    </li>
+    <li>
+      <a href='static/resume_Jun.pdf' target='_blank' rel='noreferrer'>
+        <FontAwesomeIcon icon={['fas', 'id-badge']} />
+      </a>
+    </li>
+  </ul>
+);
 
 export default SideMenu;
