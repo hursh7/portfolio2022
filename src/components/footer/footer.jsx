812d64d93be6960702dbeed8c3f0951f3a1aea12
junhp7 <junhp1994@gmail.com>
2021-12-27 20:25:50 +0900

    change link

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,52 +3,94 @@ import ContactForm from '../contact_form/contact_form';
 import styles from './footer.module.css';
 
 const Footer = ({ useScrollFadeIn, FontAwesomeIcon }) => {
-        const animatedItem = {
-            0: useScrollFadeIn('up', 1, 0.1),
-            1: useScrollFadeIn('up', 1, 0.2),
-        };
+  const animatedItem = {
+    0: useScrollFadeIn('up', 1, 0.1),
+    1: useScrollFadeIn('up', 1, 0.2),
+  };
 
-        return (
-        <footer className={styles.footer}>
-            <h2 className={styles.index}>Contact</h2>
-            <ContactForm useScrollFadeIn={useScrollFadeIn} />
-            <ul className={styles.text} {...animatedItem[0]}>
-                <li>본 사이트는 상업적 목적이 아닌 개인 포트폴리오 사이트로 제작되었습니다.</li>
-                <li>사용된 일부 이미지 및 폰트 등은 별도의 출처가 있음을 밝힙니다.</li>
-            </ul>
-            <div className={styles.ref} {...animatedItem[1]}>
-                <span>by.</span>
-                <a href="https://iconape.com/" target="_blank" rel="noreferrer" className={styles.link}>
-                    <img src={require('static/images/footer_ape.svg').default} alt="iconape" />
-                </a>
-                <a href="https://www.flaticon.com/" target="_blank" rel="noreferrer" className={styles.link}>
-                    <img src={require('static/images/footer_fla.svg').default} alt="flaticon" />
-                </a>
-                <a href="https://www.freepik.com/" target="_blank" rel="noreferrer" className={styles.link}>
-                    <img src={require('static/images/footer_freepik.svg').default} alt="freepik" />
-                </a>
-            </div>
-            <div className={styles.box}>
-                <p className={styles.copyright}>&copy; 2021 Junhyung Park. All rights reserved.</p>
-                <ul className={styles.link}>
-                    <li>
-                        <a href="https://github.com/hursh7" target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={["fab", "github"]} />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/hursh7" target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={["fas", "comment-dots"]} />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="mailto:junhp1994@gmail.com" target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={["fas", "id-badge"]} />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </footer>    
-    )};
+  return (
+    <footer className={styles.footer}>
+      <h2 className={styles.index}>Contact</h2>
+      <ContactForm useScrollFadeIn={useScrollFadeIn} />
+      <ul className={styles.text} {...animatedItem[0]}>
+        <li>
+          본 사이트는 상업적 목적이 아닌 개인 포트폴리오 사이트로
+          제작되었습니다.
+        </li>
+        <li>사용된 일부 이미지 및 폰트 등은 별도의 출처가 있음을 밝힙니다.</li>
+      </ul>
+      <div className={styles.ref} {...animatedItem[1]}>
+        <span>by.</span>
+        <a
+          href='https://iconape.com/'
+          target='_blank'
+          rel='noreferrer'
+          className={styles.link}
+        >
+          <img
+            src={require('static/images/footer_ape.svg').default}
+            alt='iconape'
+          />
+        </a>
+        <a
+          href='https://www.flaticon.com/'
+          target='_blank'
+          rel='noreferrer'
+          className={styles.link}
+        >
+          <img
+            src={require('static/images/footer_fla.svg').default}
+            alt='flaticon'
+          />
+        </a>
+        <a
+          href='https://www.freepik.com/'
+          target='_blank'
+          rel='noreferrer'
+          className={styles.link}
+        >
+          <img
+            src={require('static/images/footer_freepik.svg').default}
+            alt='freepik'
+          />
+        </a>
+      </div>
+      <div className={styles.box}>
+        <p className={styles.copyright}>
+          &copy; 2021 Junhyung Park. All rights reserved.
+        </p>
+        <ul className={styles.link}>
+          <li>
+            <a
+              href='https://github.com/hursh7'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FontAwesomeIcon icon={['fab', 'github']} />
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://www.notion.so/junhp/205e0379c9444d30b32de2ddb7672ac5'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FontAwesomeIcon icon={['fas', 'comment-dots']} />
+            </a>
+          </li>
+          <li>
+            <a
+              href='mailto:junhp1994@gmail.com'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FontAwesomeIcon icon={['fas', 'id-badge']} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,58 +5,69 @@ import styles from './header.module.css';
 const NAVIGATION_ITEMS = ['About', 'Skills', 'Projects', 'Contact'];
 
 const Header = forwardRef(({ onReset, FontAwesomeIcon }, ref) => {
-    const [showNav, setShowNav] = useState(false);
+  const [showNav, setShowNav] = useState(false);
 
-    const onToggle= () => {
-        setShowNav(showNav => !showNav);
-    }
-    
-    const toggle = showNav === false ? styles.true : styles.false;
-    const hide = showNav === false ? styles.nav : styles.hide;
+  const onToggle = () => {
+    setShowNav(showNav => !showNav);
+  };
 
-    return (
-        <header className={styles.header}>
-            <p href="" className={styles.logo} onClick={() => {
-                onReset()
-                onToggle()
-                }
-            }>
-                Jun .
-            </p>
-            <div className={`${styles.nav} ${hide}`}>
-                <ul className={styles.indexBox} ref={ref} onClick={() => onToggle()}>
-                    {NAVIGATION_ITEMS.map(item => (
-                        <NavigationItem key={item} item={item} />
-                    ))}
-                </ul>
-                <ul className={styles.link}>
-                    <li>
-                        <a href="https://github.com/hursh7" target="_blank"  rel="noreferrer">
-                            <FontAwesomeIcon icon={["fab", "github"]} />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/hursh7" target="_blank"  rel="noreferrer">
-                            <FontAwesomeIcon icon={["fas", "comment-dots"]} />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="mailto:junhp1994@gmail.com" target="_blank"  rel="noreferrer">
-                            <FontAwesomeIcon icon={["fab", "google"]} />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div className={`${styles.true} ${toggle}`} onClick={() => onToggle()}>
-                <div className={styles.trigger}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-                <div className={styles.shadow}></div>
-            </div>
-        </header>
-    )
+  const toggle = showNav === false ? styles.true : styles.false;
+  const hide = showNav === false ? styles.nav : styles.hide;
+
+  return (
+    <header className={styles.header}>
+      <p
+        href=''
+        className={styles.logo}
+        onClick={() => {
+          onReset();
+          onToggle();
+        }}
+      >
+        Jun .
+      </p>
+      <div className={`${styles.nav} ${hide}`}>
+        <ul className={styles.indexBox} ref={ref} onClick={() => onToggle()}>
+          {NAVIGATION_ITEMS.map(item => (
+            <NavigationItem key={item} item={item} />
+          ))}
+        </ul>
+        <ul className={styles.link}>
+          <li>
+            <a
+              href='https://github.com/hursh7'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FontAwesomeIcon icon={['fab', 'github']} />
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://www.notion.so/junhp/205e0379c9444d30b32de2ddb7672ac5'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FontAwesomeIcon icon={['fas', 'comment-dots']} />
+            </a>
+          </li>
+          <li>
+            <a href='static/resume_Jun.pdf' target='_blank' rel='noreferrer'>
+              <FontAwesomeIcon icon={['fas', 'id-badge']} />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className={`${styles.true} ${toggle}`} onClick={() => onToggle()}>
+        <div className={styles.trigger}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className={styles.shadow}></div>
+      </div>
+    </header>
+  );
 });
 
 export default Header;
